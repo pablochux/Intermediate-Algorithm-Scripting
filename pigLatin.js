1dffb8649f02e480c1619a0d5ba191618c1b3a40
pablochux15@gmail.com <pablochux15@gmail.com>
2016-09-05 19:21:07 +0200

    Improve pigLatin

--- pigLatin.js
+++ pigLatin.js
@@ -8,13 +8,12 @@ function translatePigLatin(str) {
     if (isVowel(str[0])) {
         newStr = str + 'way';
     } else { // If first letter is a consonant -> add way
+        // If first letter is a consonant and second letter is a consonant-> add way
         if (!isVowel(str[1])) {
             newStr = str.slice(2) + str[0] + str[1] + 'ay';
-        } else {
+        } else { // Only first letter is a consonant
             newStr = str.slice(1) + str[0] + 'ay';
         }
-        // Search the first consonant
-
     }
     return newStr;
 }
